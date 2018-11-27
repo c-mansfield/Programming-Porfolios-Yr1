07d630a901c21ec237e59621b650d7b430478d22
Cole Mansfield <colemansfield@googlemail.com>
2018-11-27 20:54:40 +0000

    ASCII Mona Lisa 2D arrays

---
+++ 2Darrays/src/main.java
@@ -0,0 +1,11 @@
+public class main {
+
+	public static void main(String[] args) {
+		Image2Ascii image2ascii = new Image2Ascii();
+		image2ascii.LoadImage("mona.png");
+
+			image2ascii.printWarhol();
+		
+	}
+
+}
